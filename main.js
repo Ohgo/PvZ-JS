19758eb30e675573b2321f023e3eaabceff3acc0
Huimin Zhang <joyzhmjoy@gmail.com>
2014-04-07 11:26:29 +0200

    -0-0-0-0-0-0 Joy

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 ssssswe
  http://www.cocos2d-x.org
 
-
+-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-00-0-0-0-0-0-0-0-0000-0-0-0-0-0
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
