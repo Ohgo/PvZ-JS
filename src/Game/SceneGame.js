c15461d64f0cec4ce205e3a4c1734c4cc82b5f3c
Huimin Zhang <joyzhmjoy@gmail.com>
2014-05-06 10:23:24 +0200

    final

--- src/Game/SceneGame.js
+++ src/Game/SceneGame.js
@@ -1,3 +1,6 @@
+var g_GameZOder = {bg:0, ui:1, front:100};//present layer
+var g_GameStatus = {play:0, stop:1, gameOver:2};//game status
+
 var SceneGame = cc.Scene.extend({
     onEnter:function () {
         cc.log("SceneGame: onEnter");
@@ -6,8 +9,8 @@ var SceneGame = cc.Scene.extend({
         var gBGLayer = new LayerGameBg();
         var gCharacterLayer = new GameCharacterLayer();
 
-        this.addChild(gBGLayer, 0);
-        this.addChild(gCharacterLayer, 1);
+        this.addChild(gBGLayer, g_GameZOder.bg);
+        this.addChild(gCharacterLayer, g_GameZOder.ui);
         //gCharacterLayer.init();
     },
 
